274acba04f62991c2eca0735caea899a37346cfc
SaniyaMuntaz <muntaz4951@gmail.com>
2026-04-24 17:12:22 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,12 @@ import mongoose from 'mongoose';
 import cors from 'cors';
 
 const app = express();
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true }
+});
 
+const User = mongoose.model('User', userSchema);
 
 app.use(cors());
 app.use(express.json());
