681d9462b71df15d877f4e928ac69d6352f7ba77
SaniyaMuntaz <muntaz4951@gmail.com>
2026-04-24 18:01:37 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ export default function App() {
   const endpoint = isRegistering ? 'register' : 'login';
   
   try {
-    const res = await fetch("https://grubgo-backend-5u6u.onrender.com/api/restaurants", {
+    const res = await fetch('https://grubgo-backend-5u6u.onrender.com/api/login', {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(loginData)
