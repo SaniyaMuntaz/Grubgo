9e62640a21f0eb491d44893ac9e05104881ae9b1
SaniyaMuntaz <muntaz4951@gmail.com>
2026-04-24 15:52:30 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -33,7 +33,7 @@ export default function App() {
   };
 
   useEffect(() => {
-    fetch("https://grubgo-4gwb.onrender.com/api/restaurants")
+    fetch("https://grubgo-4gwb.onrender.com")
       .then(res => res.json())
       .then(data => setRestaurants(data))
       .catch(err => console.error(err));
